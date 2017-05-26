87feac57561dae71d40c79e23edbab70e120eb3c
benitkibabu <benitkibabu@gmail.com>
2017-05-26 14:49:42 +0100

    added loading animation for images and when loading content

--- app/src/main/java/com/example/benitkibabu/randombeerapp/BeerActivity.java
+++ app/src/main/java/com/example/benitkibabu/randombeerapp/BeerActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.benitkibabu.randombeerapp.models.Beer;
@@ -25,6 +26,7 @@ public class BeerActivity extends AppCompatActivity {
     ImageView labelView;
     TextView title, descriptionText;
 
+    ProgressBar pb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +35,7 @@ public class BeerActivity extends AppCompatActivity {
         labelView = (ImageView) findViewById(R.id.labelView);
         title = (TextView) findViewById(R.id.title);
         descriptionText = (TextView) findViewById(R.id.descriptionText) ;
+        pb = (ProgressBar) findViewById(R.id.progressBar);
 
         getRandomBeer();
 
@@ -46,36 +49,53 @@ public class BeerActivity extends AppCompatActivity {
     }
 
     void getRandomBeer(){
+        showProgressBar();
         ApiInterface apiService = ApiClient.getRetrofit().create(ApiInterface.class);
 
         Call<SearchResult> call = apiService.getBeer(getString(R.string.BreweryAPI_KEY), "Y");
         call.enqueue(new Callback<SearchResult>() {
             @Override
             public void onResponse(Call<SearchResult> call, Response<SearchResult> response) {
-                Beer beer = response.body().getData();
-                Labels labels = beer.getLabel();
+                hideProgressBar();
+                if(response.isSuccessful()) {
+                    Beer beer = response.body().getData();
+                    Labels labels = beer.getLabel();
 
-                title.setText(beer.getName());
+                    title.setText(beer.getName());
 
-                if(beer.getDescription() == null || beer.getDescription().isEmpty())
-                    descriptionText.setText("No Description");
-                else
-                    descriptionText.setText(beer.getDescription());
+                    if (beer.getDescription() == null || beer.getDescription().isEmpty())
+                        descriptionText.setText("No Description");
+                    else
+                        descriptionText.setText(beer.getDescription());
 
-                Picasso.with(BeerActivity.this).load(labels.getLarge())
-                        .resize(512, 512)
-                        .centerCrop()
-                        .into(labelView);
+                    Picasso.with(BeerActivity.this).load(labels.getLarge())
+                            .placeholder(R.drawable.prorgress_animation)
+                            .resize(512, 512)
+                            .centerCrop()
+                            .into(labelView);
 
-                Log.d(TAG, response.body().getStatus());
+                    Log.d(TAG, response.body().getStatus());
+                }
             }
 
             @Override
             public void onFailure(Call<SearchResult> call, Throwable throwable) {
+                hideProgressBar();
                 Log.e(TAG, throwable.toString());
             }
         });
     }
+
+    void showProgressBar(){
+        if(pb !=null)
+            pb.setVisibility(View.VISIBLE);
+    }
+
+    void hideProgressBar(){
+        if(pb !=null)
+            pb.setVisibility(View.INVISIBLE);
+    }
+
 
     //When we resume the application, recall the getRandomBeer() method to generate a new random beer
     @Override
